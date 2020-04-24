8f693b009e737583e7b9d6bc98758340a6c0ff2c
Patrick Alexander Gersch <p.gersch@campus.tu-berlin.de>
2020-04-24 08:56:01 +0200

    Running Textbook on cw

--- polymul/main_cw_test.c
+++ polymul/main_cw_test.c
@@ -67,6 +67,29 @@ int main(void) {
   while(1){
     simpleserial_get();
     simpleserial_get();
+    if(key_length <= 0){
+      return 1;
+    }
+    if(text_length <= 0){
+      return 1;
+    }
+
+    result_length = key_length + text_length;
+
+    uint16_t result[result_length];
+    for(int i = 0; i < result_length; i++){
+      result[i] = 0;
+    }
+    int status = 0;
+
+    status = polynomial_multiplication(
+                key, key_length,
+                text, text_length,
+              result);
+
+    simpleserial_put('r', 2*result_length, (uint8_t *)result);
+    simpleserial_put('z', 1, (uint8_t *) &status);
+
     simpleserial_get();
     while(reset_m4 != 1){}
     reset_m4 = 0;
