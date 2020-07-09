dd737401768298993786b591f673e0ab7e5da7ce
Patrick Alexander Gersch <p.gersch@campus.tu-berlin.de>
2020-07-09 20:14:38 +0200

    Faster div3

--- polymul/libpolymath/poly_div3.c
+++ polymul/libpolymath/poly_div3.c
@@ -1,9 +1,10 @@
 #include "polymath.h"
 
+#define INVERSE_3 43691
+
 void poly_div3(uint16_t *a, int len, uint16_t *b)
 {
   for (int i = 0; i < len; i++){
-    //TODO you can do better
-    b[i] = a[i] / 3;
+    b[i] = (uint16_t)(a[i] * INVERSE_3);
   }
 }
