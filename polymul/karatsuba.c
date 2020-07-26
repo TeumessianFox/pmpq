2ee9d1d07fd0a78233fbe7cccfea405d32f56ef7
Patrick Alexander Gersch <p.gersch@campus.tu-berlin.de>
2020-07-26 22:11:48 +0200

    Adding improved and clean karatsuba implementation

--- polymul/karatsuba.c
+++ polymul/karatsuba.c
@@ -8,38 +8,39 @@ int karatsuba(
     uint16_t *result,
     int depth)
 {
-  int half_len = key_length;
-  if (half_len < text_length)
-    half_len = text_length;
-  if(half_len % 2 != 0)
-    half_len += 1;
-  int result_half = half_len;
-  half_len = half_len / 2;
+  if(key_length != text_length)
+    return 0x01;
+  int degree = key_length;
+  if(degree % 2 == 1){
+    // TODO: What todo for degrees which are not a power of 2?
+  }
+  int limb_result_len = degree - 1;
+  int limb_len = degree / 2;
 
-  uint16_t result_mid[result_half];
-  uint16_t result_upper[result_half];
-  uint16_t result_low[result_half];
-  for(int i = 0; i < result_half; i++){
-    result_mid[i] = 0;
-    result_upper[i] = 0;
-    result_low[i] = 0;
+  uint16_t result_mid[limb_result_len];
+  uint16_t result_upper[limb_result_len];
+  uint16_t result_low[limb_result_len];
+  uint16_t key_add[limb_len];
+  uint16_t text_add[limb_len];
+  for(int i = 0; i < limb_len; i++){
+    key_add[i] = key[limb_len + i] + key[i];
+    text_add[i] = text[limb_len + i] + text[i];
   }
 
-  (*chain[depth+1])(key, half_len, text, half_len, result_low, depth+1);
-  (*chain[depth+1])(key + half_len, half_len, text + half_len, half_len, result_upper, depth+1);
+  (*chain[depth+1])(key,            limb_len, text,            limb_len, result_low,   depth+1);
+  (*chain[depth+1])(key + limb_len, limb_len, text + limb_len, limb_len, result_upper, depth+1);
+  (*chain[depth+1])(key_add,        limb_len, text_add,        limb_len, result_mid,   depth+1);
 
-  uint16_t key_add[half_len];
-  uint16_t text_add[half_len];
-  for(int i = 0; i < half_len; i++){
-    key_add[i] = key[half_len + i] + key[i];
-    text_add[i] = text[half_len + i] + text[i];
+  for(int i = 0; i < limb_result_len; i++){
+    result[i] = result_low[i];
+    result[limb_result_len + i + 1] = result_upper[i];
   }
-  (*chain[depth+1])(key_add, half_len, text_add, half_len, result_mid,depth+1);
 
-  for(int i = 0; i < result_half; i++){
-    result[i] += result_low[i];
-    result[half_len + i] += result_mid[i] - result_upper[i] - result_low[i];
-    result[result_half + i] += result_upper[i];
+  int limb_result_middle = (limb_result_len - 1) / 2;
+  result[limb_result_len] = result_mid[limb_result_middle] - result_upper[limb_result_middle] - result_low[limb_result_middle];
+  for(int i = 0; i < limb_result_middle; i++){
+    result[limb_len + i] += result_mid[i] - result_upper[i] - result_low[i];
+    result[limb_result_len + i + 1] += result_mid[limb_result_middle + i + 1] - result_upper[limb_result_middle + i + 1] - result_low[limb_result_middle + i + 1];
   }
 
   return 0x00;
